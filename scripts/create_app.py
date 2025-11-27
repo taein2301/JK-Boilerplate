d80e31db8b19ddd670d1abab713d7bd14955b5da
jklee <taein2301@gmail.com>
2025-11-27 22:21:40 +0900

    chore: remove create_app.py wrapper script.

--- scripts/create_app.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import subprocess, sys, os
-
-script_path = os.path.join(os.path.dirname(__file__), "create_app.sh")
-subprocess.run(["bash", script_path] + sys.argv[1:], check=True)
